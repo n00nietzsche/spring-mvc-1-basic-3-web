7fe6b70b442ae33abf378dd3e591b60c24aac6f9
n00nietzsche <n00nietzsche@gmail.com>
2021-04-12 09:45:24 +0900

    스프링 MVC 1편 - 백엔드 웹 개발 핵심 기술 #60 웹 페이지 만들기 - 상품 등록 폼

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.PostConstruct;
@@ -46,5 +47,16 @@ public class BasicItemController {
         Item item = itemRepository.findById(itemId);
         model.addAttribute("item", item);
         return "basic/item";
+    }
+
+    @GetMapping("/add")
+    public String addForm() {
+        return "basic/addForm";
+    }
+
+    // 같은 URL이지만 HTTP 메소드로 기능을 구분해주기
+    @PostMapping("/add")
+    public void add() {
+
     }
 }
